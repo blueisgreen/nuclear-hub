f40aa120c80428cbdaca456fb13ac627993e6868
Dave Mount <davemount@gmail.com>
2023-11-17 17:39:15 -0800

    Save experiments

--- pages/what-say-you.vue
+++ pages/what-say-you.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>Nuclear Energy eXchange</div>
-  <div>
+  <h1>Nuclear Energy eXchange</h1>
+  <p>
     Let's talk about anything related to the productive use of nuclear energy.
-  </div>
-  <div>
+  </p>
+  <p>
     As fascinating as nuclear weapons might be, nothing productive comes of
     them. While the topic is not taboo per se, we will focus attention on more
     productive ideas.
-  </div>
+  </p>
 </template>
 
 <script setup lang="ts"></script>
---
+++ pages/word-search.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Word Searches</h1>
+  <p>Have fun and while learning vocabulary related to nuclear energy.</p>
+  <WordSearchPuzzle />
+</template>
