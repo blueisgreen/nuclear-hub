e4bd63e5c3dddfea5b240b7fd798d74504a7471d
Dave Mount <davemount@gmail.com>
2023-11-18 17:05:27 -0800

    Add nav

--- App.vue
+++ App.vue
@@ -1,5 +1,15 @@
 <template>
+  <nav>
+    TOC: <a href="/">Home</a> | <a href="/plans">Plans</a> |
+    <a href="/what-say-you">Discussion</a> | <a href="/word-search">Games</a>
+    <hr />
+  </nav>
   <div>
     <NuxtPage />
   </div>
 </template>
+<style>
+nav {
+  margin-bottom: 2rem;
+}
+</style>
